74660dd9d2f2fcdab9851d36560576e7e297d973
Arunkumar <arunkumar.msg1982@gmail.com>
2024-05-08 21:31:42 +0530

    remove toast

--- app/src/main/java/com/rcd/calculaterapp/MainActivity.kt
+++ app/src/main/java/com/rcd/calculaterapp/MainActivity.kt
@@ -5,160 +5,110 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-
-
-
 
 class MainActivity : AppCompatActivity() {
     var usInput: TextView? = null
-    var dote : TextView? = null
-    var remove : TextView? = null
-
+    var dote: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        usInput  = findViewById(R.id.display)
-        dote=findViewById(R.id.dot)
-        remove=findViewById(R.id.rougn)
+        usInput = findViewById(R.id.display)
+        dote = findViewById(R.id.dot)
     }
 
-    fun onDigit(view: View){
-        Toast.makeText(this, "Button - ${(view as Button).text} Tapped", Toast.LENGTH_LONG).show()
-        usInput?.append(view.text)
+    fun onDigit(view: View) {
+        usInput?.append((view as Button).text)
     }
 
-    fun cancel(cancel: View){
-        Toast.makeText(this, "Button - ${(cancel as Button).text}ancel Tapped", Toast.LENGTH_LONG).show()
-        usInput?.text=""
+    fun cancel(cancel: View) {
+        usInput?.text = ""
         dote?.isClickable = true
     }
 
-    fun dot(dot:View){
-        Toast.makeText(this, "Button - ${(dot as Button).text} Tapped", Toast.LENGTH_LONG).show()
-        usInput?.append(dot.text)
+    fun dot(dot: View) {
+        usInput?.append((dot as Button).text)
         dote?.isClickable = false
     }
 
     fun delete(view: View) {
-        usInput?.text.toString()
-        usInput?.text= usInput?.text?.dropLast(1)
+        usInput?.text = usInput?.text?.dropLast(1)
     }
 
-    fun operator(operator:View){
-        Toast.makeText(this, "Button - ${(operator as Button).text} Tapped", Toast.LENGTH_LONG).show()
-        usInput?.append(operator.text)
+    fun operator(operator: View) {
+        usInput?.append((operator as Button).text)
         dote?.isClickable = true
     }
 
-
-    fun remove0valAtEndIndex(answer:Double): String{
-
+    fun remove0valAtEndIndex(answer: Double): String {
         var result = answer.toString()
-
-        if(result.substring(result.length - 2, result.length) == ".0")
-        {
+        if (result.substring(result.length - 2, result.length) == ".0") {
             return result.substring(0, result.length - 2)
-        }
-        else
-        {
+        } else {
             return result
         }
-
     }
 
-    fun equal(equal:View){
-
+    fun equal(equal: View) {
         var userVal = (usInput?.text).toString()
         var prefix = ""
-        if(userVal.startsWith("-")){
+        if (userVal.startsWith("-")) {
             prefix = "-"
-            userVal=userVal.substring(1)
+            userVal = userVal.substring(1)
         }
 
-        if(userVal.contains("+")) {
-            var userInput = userVal.split("+")
-
-            var firstVal = userInput[0]
-            if(prefix=="-"){
-                firstVal=prefix+userInput[0]
+        when {
+            userVal.contains("+") -> {
+                val userInput = userVal.split("+")
+                var firstVal = userInput[0]
+                if (prefix == "-") {
+                    firstVal = prefix + userInput[0]
+                }
+                val output = firstVal.toDouble() + userInput[1].toDouble()
+                usInput?.text = remove0valAtEndIndex(output)
             }
-            var output = firstVal.toDouble() + userInput[1].toDouble()
-            usInput?.text = remove0valAtEndIndex(output)
-
-        }
-
-
-        else if(userVal.contains("-")) {
-            var userInput = userVal.split("-")
-            var firstVal = userInput[0]
-            if(prefix=="-"){
-                firstVal=prefix+userInput[0]
+            userVal.contains("-") -> {
+                val userInput = userVal.split("-")
+                var firstVal = userInput[0]
+                if (prefix == "-") {
+                    firstVal = prefix + userInput[0]
+                }
+                val output = firstVal.toDouble() - userInput[1].toDouble()
+                usInput?.text = remove0valAtEndIndex(output)
             }
-            var output = firstVal.toDouble() - userInput[1].toDouble()
-            usInput?.text = remove0valAtEndIndex(output)
-        }
-
-
-        else if(userVal.contains("*")) {
-            var userInput = userVal.split("*")
-            var firstVal = userInput[0]
-            if(prefix=="-"){
-                firstVal=prefix+userInput[0]
+            userVal.contains("*") -> {
+                val userInput = userVal.split("*")
+                var firstVal = userInput[0]
+                if (prefix == "-") {
+                    firstVal = prefix + userInput[0]
+                }
+                val output = firstVal.toDouble() * userInput[1].toDouble()
+                usInput?.text = remove0valAtEndIndex(output)
             }
-            var output = firstVal.toDouble() * userInput[1].toDouble()
-            usInput?.text = remove0valAtEndIndex(output)
-        }
-
-
-        else if(userVal.contains("/")) {
-            var userInput = userVal.split("/")
-            var firstVal = userInput[0]
-            if(prefix=="-"){
-                firstVal=prefix+userInput[0]
+            userVal.contains("/") -> {
+                val userInput = userVal.split("/")
+                var firstVal = userInput[0]
+                if (prefix == "-") {
+                    firstVal = prefix + userInput[0]
+                }
+                val output = firstVal.toDouble() / userInput[1].toDouble()
+                usInput?.text = remove0valAtEndIndex(output)
             }
-            var output = firstVal.toDouble() / userInput[1].toDouble()
-            usInput?.text = remove0valAtEndIndex(output)
         }
-
-
-
-
     }
 
-    fun root(root:View){
+    fun root(root: View) {
         var userVal = (usInput?.text).toString()
         var prefix = ""
-        if(userVal.startsWith("-")){
+        if (userVal.startsWith("-")) {
             prefix = "-"
-            userVal=userVal.substring(1)
+            userVal = userVal.substring(1)
         }
         var firstVal = userVal
-        if(prefix=="-"){
-            firstVal=prefix+firstVal
+        if (prefix == "-") {
+            firstVal = prefix + firstVal
         }
-        var output = firstVal.toDouble() * firstVal.toDouble()
+        val output = firstVal.toDouble() * firstVal.toDouble()
         usInput?.text = remove0valAtEndIndex(output)
-
-
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
